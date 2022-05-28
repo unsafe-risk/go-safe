e36eebbcfd5de6b41d5c2241c3a22a8cae05959b
Lee Jaeseong <fg9658@gmail.com>
2022-05-28 15:53:23 +0900

    feat: 숫자 관련 기본값 함수 및 숫자 관련 제로값 함수 추가, 기존 레거시 함수 인터페이스로만 존재 (전부 새로운 함수를 호출하게 변경)

--- default.go
+++ default.go
@@ -18,86 +18,66 @@ func StrOrDefault(s *string, def func() string) string {
 	return *s
 }
 
-func IOrDefault(n *int, def func() int) int {
+func NumOrDefault[T number](n *T, def func() T) T {
 	if n == nil {
 		return def()
 	}
+
 	return *n
+}
+
+func Safe[T Safety[T]](in T) T {
+	if in.IsNil() {
+		return in.Default()
+	}
+
+	return in
+}
+
+func IOrDefault(n *int, def func() int) int {
+	return NumOrDefault(n, def)
 }
 
 func I8OrDefault(n *int8, def func() int8) int8 {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
 
 func I16OrDefault(n *int16, def func() int16) int16 {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
 
 func I32OrDefault(n *int32, def func() int32) int32 {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
 
 func I64OrDefault(n *int64, def func() int64) int64 {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
 
 func UiOrDefault(n *uint, def func() uint) uint {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
 
 func Ui8OrDefault(n *uint8, def func() uint8) uint8 {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
 
 func Ui16OrDefault(n *uint16, def func() uint16) uint16 {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
 
 func Ui32OrDefault(n *uint32, def func() uint32) uint32 {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
 
 func Ui64OrDefault(n *uint64, def func() uint64) uint64 {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
 
 func F32OrDefault(n *float32, def func() float32) float32 {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
 
 func F64OrDefault(n *float64, def func() float64) float64 {
-	if n == nil {
-		return def()
-	}
-	return *n
+	return NumOrDefault(n, def)
 }
